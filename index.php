19e31949aacae0b523e8b7601ca3cc469065b330
Michael <mtrletcher@hotmail.com>
2013-05-31 20:23:07 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<title>Dog-company</title>
-	<title><?php wp_title( '-', true, 'right' ); echo esc_html( get_bloginfo('name') ); ?></title>
-	<meta charset="<?php bloginfo('charset') ?>" />
-	<meta http-equiv="content-type" content="text/html;charset=UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-	<!-- Modernizr -->
-	<script src="./wp-content/themes/dog-company/js/libs/modernizr-2.6.2.min.js"></script>
-	<!-- jQuery -->
-	<script type="text/javascript" src="./wp-content/themes/dog-company/js/libs/jquery-1.9.1.min.js"></script>
-	<!-- GroundworkCSS -->
-	<link type="text/css" rel="stylesheet" href="./wp-content/themes/dog-company/css/groundwork.css">
-	<!--[if IE]>
-	<link type="text/css" rel="stylesheet" href="./wp-content/themes/dog-company/css/groundwork-ie.css">
-	<![endif]-->
-	<!--[if lt IE 9]>
-	<script type="text/javascript" src="./wp-content/themes/dog-company/js/libs/html5shiv.min.js"></script>
-	<![endif]-->
-	<!--[if IE 7]>
-	<link type="text/css" rel="stylesheet" href="./wp-content/themes/dog-company/css/font-awesome-ie7.min.css">
-	<![endif]-->
-	<link type="text/css" rel="stylesheet" href="./wp-content/themes/dog-company/css/style.css" />
-	<script type="text/javascript">
+		<title>Dog-Company</title>
+		<title><?php wp_title( '-', true, 'right' ); echo esc_html( get_bloginfo('name') ); ?></title>
+		<meta charset="<?php bloginfo('charset') ?>" />
+		<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+		<!-- Modernizr -->
+		<script src="./wp-content/themes/dog-company/js/libs/modernizr-2.6.2.min.js"></script>
+		<!-- jQuery -->
+		<script type="text/javascript" src="./wp-content/themes/dog-company/js/libs/jquery-1.9.1.min.js"></script>
+		<!-- DC Style -->
+		<link type="text/css" rel="stylesheet" href="./wp-content/themes/dog-company/style.css">
+		<!-- GroundworkCSS -->
+		<link type="text/css" rel="stylesheet" href="./wp-content/themes/dog-company/css/groundwork.css">
+		<!--[if IE]>
+		<link type="text/css" rel="stylesheet" href="./wp-content/themes/dog-company/css/groundwork-ie.css">
+		<![endif]-->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="./wp-content/themes/dog-company/js/libs/html5shiv.min.js"></script>
+		<![endif]-->
+		<!--[if IE 7]>
+		<link type="text/css" rel="stylesheet" href="./wp-content/themes/dog-company/css/font-awesome-ie7.min.css">
+		<![endif]-->
+		<script type="text/javascript">
 				function fix_top_bar(){
 					var barWidth = $('.nav-fixed').width();
 					$('.nav-fixed').css({ 'left' : '50%', 'margin-left' : '-' + (barWidth/2 + 20) + 'px' });
@@ -50,33 +51,31 @@
 					});
 
 	</script>
-</head>
+	</head>
 	<body>
 		<!-- header starts here -->
 		<div id="container">
 			<header>
-				<article id="logo" class="container centered padded double gap-bottom">
+				<article id="logo" class="container centered padded double">
 					<img src="./wp-content/themes/dog-company/images/DC_white.png" width="400"></img>
 				</article>
-				<article id="countdown"  class="container centered">
-				</article>
-				<nav class="row container double pad-right pad-left">
-					<ul class="pull-left">
-					<a href="./">Home</a>
-					<a>Main Menu</a>
-					<a>Forums</a>
-					<a href="./video">Video</a>
+				<nav class="row container double pad-right pad-left" >
+					<ul id="pull-left">
+					<a id="menu" href="./">hoMe</a>
+					<a id="menu" href="">huB</a>
+					<a id="menu" href="">Forums</a>
+					<a id="menu" href="">video</a>
 					</ul>
+					
 					<ul class="pull-right">
-					<a>Sign in</a>
-					<a>Register</a>
+					<a id="menu" href="">Sign in</a>
+					<a id="menu" href="">Register</a>
 					</ul>
 				</nav>
 			</header>
 			<!-- end of header -->
 			<!-- start of horizontal sidebar -->
 			<section id="horizontal-sidebar" class="pad-bottom">
-
 			</section>
 			<!-- end horizontal sidebar -->
 			<!-- start main content -->
