b3bce63790de86c19bef82fd3ada2aa4ce2bfe4b
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-03 15:33:16 -0400

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+/*remove admin bar */
+show_admin_bar(false);
+
+
+/* register sidebar*/
+function arphabet_widgets_init() {
+
+
+
+
+if ( function_exists('register_sidebar') )
+    if ( function_exists('register_sidebar') )
+        register_sidebar(array('name'=>'sidebar', //Name your sidebar
+        'description' => 'These widgets will appear in the Right sidebar.',
+        'before_widget' => '<div class="widget">', // Displays before widget
+        'after_widget' => '</div>', // Displayed after widget
+        'before_title' => '<h3>', //Displays before title, after widget start
+        'after_title' => '</h3>' //Displays after title
+    ));
+  //endif;
+    if ( function_exists('register_sidebar') )
+        register_sidebar(array('name'=>'slider', //Name your sidebar
+        'description' => 'Slider goes here',
+        'before_widget' => '<div class="pull-left">', // Displays before widget
+        'after_widget' => '</div>', // Displayed after widget
+        'before_title' => '<h3>', //Displays before title, after widget start
+        'after_title' => '</h3>' //Displays after title
+    ));	
+	if ( function_exists('register_sidebar') )
+    register_sidebar(array('name'=>'smallsidebar', //Name your sidebar
+    'description' => 'Sidebar next to slider',
+    'before_widget' => '<div class="pull-left">', // Displays before widget
+    'after_widget' => '</div>', // Displayed after widget
+    'before_title' => '<h3>', //Displays before title, after widget start
+    'after_title' => '</h3>' //Displays after title
+    ));	
+	}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+	?>
