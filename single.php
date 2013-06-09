6d3d782ffc73f02b90c9cee6faa4e4d908961086
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-09 16:34:19 -0500

    Create single.php

---
+++ single.php
@@ -0,0 +1,8 @@
+  <?php include(TEMPLATEPATH.'/header.php'); ?>
+	<!-- start main content -->
+		<section id="content"  class="container centered whole padded">
+			<?php include(TEMPLATEPATH.'/content-single.php'); ?>
+			<?php include(TEMPLATEPATH.'/sidebar.php'); ?>
+			</article>
+		</section>  
+	<?php include(TEMPLATEPATH.'/footer.php'); ?>
