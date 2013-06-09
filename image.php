10f9e2aa101148cc12e1f9f8da4a0c1c87bd89fc
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-09 17:33:47 -0400

    Create image.php

---
+++ image.php
@@ -0,0 +1,11 @@
+  <?php include(TEMPLATEPATH.'/header.php'); ?>
+	<?php include(TEMPLATEPATH.'/slider.php'); ?>
+	<!-- start main content -->
+		<section id="content"  class="container centered whole padded">
+			<?php include(TEMPLATEPATH.'/content.php'); ?>
+			<?php include(TEMPLATEPATH.'/sidebar.php'); ?>
+			</article>
+		</section>  
+	<?php include(TEMPLATEPATH.'/footer.php'); ?>
+
+			
