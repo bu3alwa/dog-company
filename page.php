4b1c24243b706e22584822816cbc79c69f69c066
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-09 16:34:38 -0500

    Create page.php

---
+++ page.php
@@ -0,0 +1,10 @@
+  <?php include(TEMPLATEPATH.'/header.php'); ?>
+	<!-- start main content -->
+		<section id="content"  class="container centered whole padded">
+			<?php include(TEMPLATEPATH.'/content.php'); ?>
+			<?php include(TEMPLATEPATH.'/sidebar.php'); ?>
+			</article>
+		</section>  
+	<?php include(TEMPLATEPATH.'/footer.php'); ?>
+
+			
