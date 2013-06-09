56a140a837d2082db5ae9e08f61387e7357a848b
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-09 17:34:57 -0400

    Create smallSidebar.php

---
+++ smallSidebar.php
@@ -0,0 +1,5 @@
+<article class="one third">
+  		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('smallsidebar') ) : ?>
+						<!-- static content goes here if sidebar is inactive -->
+			<?php endif; ?>
+			</article>
