cdcadfd5526415c09f2b1bfdfbbe21e4b4e5a94a
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-09 17:36:20 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- start footer -->
+<footer class="row padded">
+  			<article id="footer-content" class="container">
+				<ul>
+				<li>test 1</li>
+				<li>test 1</li>
+				<li>test 1</li>
+				</ul>
+				</article>
+			</footer>
+			<!-- end footer -->  
+		</div>
+	</body>
+</html>
