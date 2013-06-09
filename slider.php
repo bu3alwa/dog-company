e428ecbf87c71e86a535c39d453e0b0cfcfbf89c
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-09 16:35:16 -0500

    Create slider.php

---
+++ slider.php
@@ -0,0 +1,13 @@
+  		<!-- start of horizontal sidebar -->
+			<section id="slider" class="pad-bottom">
+			<div class="container">
+			<article class="two third">
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('slider') ) : ?>
+						<!-- static content goes here if sidebar is inactive -->
+			<?php endif; ?>
+			</article>
+			<?php include(TEMPLATEPATH.'/smallSidebar.php'); ?>
+			<div>
+			</section>
+
+			<!-- end horizontal sidebar -->
