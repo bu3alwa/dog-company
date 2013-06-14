dea452cc9dce5aab3b71512f3798a0ecfb54553f
Michael <mtrletcher@hotmail.com>
2013-06-14 16:34:47 +1100

    Update slider.php

--- slider.php
+++ slider.php
@@ -1,13 +1,12 @@
-  		<!-- start of horizontal sidebar -->
-			<section id="slider" class="pad-bottom">
-			<div class="container">
-			<article class="two third">
+<!-- start of horizontal sidebar -->
+<section id="slider-wrapper" class="pad-bottom">
+	<div class="container">
+		<article id="slider" class="two third">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('slider') ) : ?>
-						<!-- static content goes here if sidebar is inactive -->
+			<!-- static content goes here if sidebar is inactive -->
 			<?php endif; ?>
-			</article>
-			<?php include(TEMPLATEPATH.'/smallSidebar.php'); ?>
-			</div>
-			</section>
-
-			<!-- end horizontal sidebar -->
+		</article>
+		<?php include(TEMPLATEPATH.'/smallSidebar.php'); ?>
+	</div>
+</section>
+<!-- end horizontal sidebar -->
