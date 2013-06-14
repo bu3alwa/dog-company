ee63c369a8fa1aaa014db02201ae96cfd27e3a1d
Michael <mtrletcher@hotmail.com>
2013-06-14 16:22:06 +1000

    Update slider.php

--- slider.php
+++ slider.php
@@ -1,9 +1,10 @@
 <!-- start of horizontal sidebar -->
 <section id="slider-wrapper" class="pad-bottom">
 	<div class="container">
-		<article id="slider" class="two third">
+		<article id="madmans-slider" class="two third">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('slider') ) : ?>
 			<!-- static content goes here if sidebar is inactive -->
+			<?php if (function_exists('nivoslider4wp_show')) { nivoslider4wp_show(); } ?>
 			<?php endif; ?>
 		</article>
 		<?php include(TEMPLATEPATH.'/smallSidebar.php'); ?>
