dcb5ce6e7195f4466e6e03668cab6fcc0d9cb3b0
Michael <mtrletcher@hotmail.com>
2013-06-22 21:25:24 +1000

    Update footer.php
    updated

--- footer.php
+++ footer.php
@@ -1,14 +1,20 @@
-<!-- start footer -->
-<footer class="row padded">
-  			<article id="footer-content" class="container">
-				<ul>
-				<li>test 1</li>
-				<li>test 1</li>
-				<li>test 1</li>
-				</ul>
+			<!-- start footer -->
+			<footer class="row padded">
+				<article id="footer-content" class="container centered">
+					<article id="logo">
+					<img src="./wp-content/themes/dog-company/images/logo_tank.png" alt="dog company tank logo" width="150" />
+					<img src="./wp-content/themes/dog-company/images/logo.png" alt="dog company logo" width="200" />
+					<img src="./wp-content/themes/dog-company/images/logo_cog.png" alt="dog company infantry logo" width="150" />
+					</article>
+					<article id="copyright">
+					<br>
+					Designed by Brad "Gumby" Doyle, Created by the Dog Company Team
+					<br>
+					<i>Copyright 2013 by Dog Company. All Rights Reserved.</i>
+					</article>
 				</article>
 			</footer>
-			<!-- end footer -->  
+			<!-- end footer -->
 		</div>
 	</body>
 </html>
