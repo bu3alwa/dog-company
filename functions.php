7758ea147441c2d0d6d1277ed059384ff3b6495c
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-24 00:07:55 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ if ( function_exists('register_sidebar') )
     if ( function_exists('register_sidebar') )
         register_sidebar(array('name'=>'sidebar', //Name your sidebar
         'description' => 'These widgets will appear in the Right sidebar.',
-        'before_widget' => '<div class="login">', // Displays before widget
+        'before_widget' => '<div class="sidebar">', // Displays before widget
         'after_widget' => '</div>', // Displayed after widget
         'before_title' => '<h3>', //Displays before title, after widget start
         'after_title' => '</h3>' //Displays after title
@@ -35,14 +35,6 @@ if ( function_exists('register_sidebar') )
         'after_widget' => '</div>', // Displayed after widget
         'before_title' => '<h3>', //Displays before title, after widget start
         'after_title' => '</h3>' //Displays after title
-    ));	
-	if ( function_exists('register_sidebar') )
-    register_sidebar(array('name'=>'smallsidebar', //Name your sidebar
-    'description' => 'Sidebar next to slider',
-    'before_widget' => '<div class="pull-left">', // Displays before widget
-    'after_widget' => '</div>', // Displayed after widget
-    'before_title' => '<h3>', //Displays before title, after widget start
-    'after_title' => '</h3>' //Displays after title
     ));	
 	}
 add_action( 'widgets_init', 'arphabet_widgets_init' );
