9998310371611a087c909c651c7ae4591458f996
madmanali93 <ali.bu3alwa@gmail.com>
2013-06-28 12:21:43 -0500

    Update slider.php

--- slider.php
+++ slider.php
@@ -1,13 +1,12 @@
 <!-- start of horizontal sidebar -->
-<section id="slider-wrapper" class="pad-bottom">
+<section id="slider-wrapper" >
 	<div class="container">
-		<article id="madmans-slider" class="two third">
+		<article id="madmans-slider" class="whole">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('slider') ) : ?>
 			<!-- static content goes here if sidebar is inactive -->
 			<?php if (function_exists('nivoslider4wp_show')) { nivoslider4wp_show(); } ?>
 			<?php endif; ?>
 		</article>
-		<?php include(TEMPLATEPATH.'/smallSidebar.php'); ?>
 	</div>
 </section>
 <!-- end horizontal sidebar -->
