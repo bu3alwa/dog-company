856ef6dbfd1ed496e1b28b044894565cec923262
Michael <mtrletcher@hotmail.com>
2014-01-06 21:16:40 +1100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,22 @@
 			<!-- start footer -->
-			<footer class="row padded">
-				<article id="footer-content" class="container centered">
-					<article id="logo">
-					<img src="./wp-content/themes/dog-company/images/footer.png" alt="dog company logo" />
+			<footer class="row">
+			<div class="footer-strip" ></div>
+				<div id="wrapfooter">
+					<article id="footer-content" class="container centered padded">
+						<article id="logo">
+						<img src="/images/footer.png" title="" alt="dog company footer img"></img>
+						</article>
+						<article id="copyright">
+						<br>
+						Designed by Brad "Gumby" Doyle, Created by the Dog Company Team.
+						<br>
+						<i>Copyright 2013 by Dog Company. All Rights Reserved.</i>
+						</article>
 					</article>
-					<article id="copyright">
-					<br>
-					Designed by Brad "Gumby" Doyle, Created by the Dog Company Team.
-					<br>
-					<i>Copyright 2013 by Dog Company. All Rights Reserved.</i>
-					</article>
-				</article>
+				</div>
 			</footer>
 			<!-- end footer -->
-		</div>
-		<?php wp_footer(); ?>
+		</div><!-- end container -->
+		<div id="wp-footer"><?php wp_footer(); ?></div>
 	</body>
 </html>
