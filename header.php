fb6c9427cfd13416925b2f8d8482472d1e9771b9
Michael <mtrletcher@hotmail.com>
2014-03-18 21:22:24 +1100

    Update header.php
    Googel Analytics updated

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', 'UA-41892420-3', 'dog-company.com');
+		ga('create', 'UA-41892420-1', 'dog-company.com');
 		ga('send', 'pageview');
 		</script>
 		<!-- jQuery -->
